14b028cc4c2a826389f04b27af860dd09e8e640f
ozgebektas <ozgebektas53@gmail.com>
2023-08-12 10:36:58 +0300

    dsd

---
+++ Services/Order/Core/CasgemMicroservice.Services.Order.Core.Application/Features/CQRS/Queries/GetByIdOrderingQueryRequest.cs
@@ -0,0 +1,20 @@
+﻿using CasgemMicroservice.Services.Order.Core.Application.Dtos.OrderDtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Services.Order.Core.Application.Features.CQRS.Queries
+{
+    public class GetByIdOrderingQueryRequest:IRequest<ResultOrderDto>
+    {
+        public int Id { get; set; }
+
+        public GetByIdOrderingQueryRequest(int id)
+        {
+            Id = id;
+        }
+    }
+}
